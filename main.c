85e83ca81bf121822621141fccb75a17ad363341
Thomas Selwyn <thomas@selwyn.tech>
2025-11-07 11:12:49 -0500

    Setup cmake

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    printf("SYSC 4001 - Assignment 2 - Part 2!\n");
+    pid_t pid = fork();
     return 0;
 }
