48dc548414b6c69841f6ff4299af54988aedc733
Thomas Selwyn <thomas@selwyn.tech>
2025-11-07 11:21:50 -0500

    Running two concurrent processes

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <unistd.h>
+#define TRUE 1
+#define FALSE 0
 
 int main(void) {
     printf("SYSC 4001 - Assignment 2 - Part 2!\n");
-    pid_t pid = fork();
-    return 0;
+
+    const pid_t pid = fork();
+    if (pid == 0) {
+        printf("Child process created\n");
+        uint32_t count = 0;
+        while (TRUE) {
+            printf("Children count: %d\n", count++);
+            usleep(500000);
+        }
+    } else {
+        uint32_t count = 0;
+        while (TRUE) {
+            printf("Parents  count: %d\n", count++);
+            usleep(500000);
+        }
+    }
 }
